a906fd2de7e2e1009afdb143d5faad0c2c46879d
Muhammad Faiz Al Ghozi <muhammadfaizalghozi1@gmail.com>
2022-07-21 13:14:10 +0700

    set up project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
