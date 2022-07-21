7fa8f1c59bbcf98fe2a7b77e4215f3ab010f26e5
Muhammad Faiz Al Ghozi <muhammadfaizalghozi1@gmail.com>
2022-07-21 16:16:44 +0700

    setup project

---
+++ lib/screen/events.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Event extends StatefulWidget {
+  const Event({super.key});
+
+  @override
+  State<Event> createState() => _EventState();
+}
+
+class _EventState extends State<Event> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: SingleChildScrollView()),
+    );
+  }
+}
---
+++ lib/service/api_service.dart
@@ -0,0 +1,9 @@
+import 'package:get_storage/get_storage.dart';
+import 'package:http/http.dart' as http;
+
+
+final storage = GetStorage();
+
+class ApiService {
+  
+}
